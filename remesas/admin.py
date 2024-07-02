4507365e4dbd744400ad46776d0bc1df5894788e
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2024-07-01 22:30:53 -0400

    INIT

---
+++ remesas/admin.py
@@ -0,0 +1,147 @@
+from django import forms
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from .models import Cliente, Transaccion
+from django.core.exceptions import ValidationError
+
+
+class MonthListFilter(admin.SimpleListFilter):
+    title = _("mes")
+    parameter_name = "month"
+
+    def lookups(self, request, model_admin):
+        months = [
+            (i, timezone.now().replace(month=i).strftime("%B")) for i in range(1, 13)
+        ]
+        return months
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(agregado__month=self.value())
+        return queryset
+
+
+class YearListFilter(admin.SimpleListFilter):
+    title = _("año")
+    parameter_name = "year"
+
+    def lookups(self, request, model_admin):
+        current_year = timezone.now().year
+        years = [(i, str(i)) for i in range(current_year - 1, current_year + 1)]
+        return years
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(agregado__year=self.value())
+        return queryset
+
+
+class MultiSelectEntityFilter(admin.SimpleListFilter):
+    title = _("entidad")
+    parameter_name = "entidad"
+
+    def lookups(self, request, model_admin):
+        entidades = set(Transaccion.objects.values_list("entidad", flat=True))
+        return [(entidad, entidad) for entidad in entidades]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            entidades = self.value()
+            return queryset.filter(entidad__in=entidades)
+        return queryset
+
+    def value(self):
+        value = super().value()
+        return value.split(",") if value else []
+
+
+class TransaccionForm(forms.ModelForm):
+    class Meta:
+        model = Transaccion
+        fields = "__all__"
+
+    def clean_num_comprobante(self):
+        num_comprobante = self.cleaned_data.get("num_comprobante")
+        if (
+            num_comprobante
+            and Transaccion.objects.filter(num_comprobante=num_comprobante).exists()
+        ):
+            raise ValidationError("El número de comprobante ya existe.")
+        return num_comprobante
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["cedula", "nombre", "apellido", "whatsapp"]
+    search_fields = ["cedula", "nombre", "apellido"]
+
+
+@admin.register(Transaccion)
+class TransaccionAdmin(admin.ModelAdmin):
+    form = TransaccionForm
+    list_display = [
+        "cliente",
+        "entidad",
+        "monto",
+        "estado",
+        "agregado",
+        "imagen_comprobante_preview",
+        "cliente_whatsapp",
+    ]
+    list_filter = ["estado", MultiSelectEntityFilter, YearListFilter, MonthListFilter]
+    search_fields = [
+        "cliente__cedula",
+        "cliente__nombre",
+        "cliente__apellido",
+        "entidad",
+    ]
+    autocomplete_fields = ["cliente"]
+
+    def imagen_comprobante_preview(self, obj):
+        if obj.imagen_comprobante:
+            return format_html(
+                '<a href="{0}" class="preview-link" target="_blank" data-toggle="modal" data-target="#comprobanteModal"><img src="{0}" width="50" height="50" /></a>',
+                obj.imagen_comprobante.url,
+            )
+        return "Sin Foto"
+
+    imagen_comprobante_preview.short_description = "Comprobante"
+
+    def cliente_whatsapp(self, obj):
+        return format_html(
+            '<a href="https://wa.me/{0}"target="_blank">Enviar Whatsapp</a>',
+            obj.cliente.whatsapp,
+        )
+
+    actions = ["marcar_aprobadas", "marcar_rechazadas", "marcar_pendientes"]
+
+    def marcar_aprobadas(self, request, queryset):
+        updated = queryset.update(estado="A")
+        self.message_user(
+            request, f"Se marcaron como Aprobadas {updated} transacciones."
+        )
+
+    marcar_aprobadas.short_description = _("Marcar como Aprobadas")
+
+    def marcar_rechazadas(self, request, queryset):
+        updated = queryset.update(estado="R")
+        self.message_user(
+            request, f"Se marcaron como Rechazadas {updated} transacciones."
+        )
+
+    marcar_rechazadas.short_description = _("Marcar como Rechazadas")
+
+    def marcar_pendientes(self, request, queryset):
+        updated = queryset.update(estado="P")
+        self.message_user(
+            request, f"Se marcaron como Pendientes {updated} transacciones."
+        )
+
+    marcar_pendientes.short_description = _("Marcar como Pendientes")
+
+
+admin.site.site_header = "PLAYNET"
+admin.site.site_title = "PLAYNET"
+admin.site.index_title = "Bienvenido a la Página de Administración"
---
+++ remesas/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50, blank=True, null=True)
+    cedula = models.CharField(max_length=20)
+    whatsapp = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.cedula} {self.nombre.upper()} {self.apellido.upper()}"
+
+
+class Transaccion(models.Model):
+    entidades = [
+        ("BANCOP S.A.", "BANCOP S.A."),
+        ("Banco Atlas S.A.", "Banco Atlas S.A."),
+        ("Banco BASA", "Banco BASA"),
+        ("Banco Continental S.A.E.C.A.", "Banco Continental S.A.E.C.A."),
+        ("Banco Familiar S.A.E.C.A.", "Banco Familiar S.A.E.C.A."),
+        ("Banco GNB - Paraguay", "Banco GNB - Paraguay"),
+        ("Banco Itaú Paraguay S.A.", "Banco Itaú Paraguay S.A."),
+        ("Banco Nacional de Fomento (BNF)", "Banco Nacional de Fomento (BNF)"),
+        ("Banco Rio S.A.E.C.A", "Banco Rio S.A.E.C.A"),
+        ("COOPEDUC Ltda.", "COOPEDUC Ltda."),
+        ("COPACONS Ltda.", "COPACONS Ltda."),
+        ("Cooperativa 21 de Setiembre Ltda.", "Cooperativa 21 de Setiembre Ltda."),
+        ("Cooperativa Alemán Concordia Ltda.", "Cooperativa Alemán Concordia Ltda."),
+        ("Cooperativa Chortitzer Ltda.", "Cooperativa Chortitzer Ltda."),
+        ("Cooperativa Coodeñe Ltda.", "Cooperativa Coodeñe Ltda."),
+        ("Cooperativa Colonias Unidas Ltda.", "Cooperativa Colonias Unidas Ltda."),
+        ("Cooperativa Coomecipar Ltda.", "Cooperativa Coomecipar Ltda."),
+        (
+            "Cooperativa de las Fuerzas Armadas de la Nación Ltda.",
+            "Cooperativa de las Fuerzas Armadas de la Nación Ltda.",
+        ),
+        ("Cooperativa Fernheim Ltda.", "Cooperativa Fernheim Ltda."),
+        ("Cooperativa Mborayhu Ltda.", "Cooperativa Mborayhu Ltda."),
+        ("Cooperativa Mburicao Ltda.", "Cooperativa Mburicao Ltda."),
+        ("Cooperativa Medalla Milagrosa Ltda.", "Cooperativa Medalla Milagrosa Ltda."),
+        ("Cooperativa Mercado 4 Ltda.", "Cooperativa Mercado 4 Ltda."),
+        (
+            "Cooperativa Multiactiva 8 de Marzo Ltda.",
+            "Cooperativa Multiactiva 8 de Marzo Ltda.",
+        ),
+        ("Cooperativa Naranjal Ltda.", "Cooperativa Naranjal Ltda."),
+        ("Cooperativa Nazareth Ltda.", "Cooperativa Nazareth Ltda."),
+        ("Cooperativa Neuland Ltda.", "Cooperativa Neuland Ltda."),
+        ("Cooperativa Ñemby Ltda.", "Cooperativa Ñemby Ltda."),
+        ("Cooperativa Raúl Peña Ltda.", "Cooperativa Raúl Peña Ltda."),
+        ("Cooperativa San Ignacio", "Cooperativa San Ignacio"),
+        ("Cooperativa San Juan Bautista Ltda.", "Cooperativa San Juan Bautista Ltda."),
+        ("Cooperativa Universitaria Ltda.", "Cooperativa Universitaria Ltda."),
+        ("Cooperativa Ypacarai Ltda.", "Cooperativa Ypacarai Ltda."),
+        (
+            "Financiera Paraguayo Japonesa S.A.E.C.A",
+            "Financiera Paraguayo Japonesa S.A.E.C.A",
+        ),
+        ("FINANCIERA FIC S.A.E.C.A.", "FINANCIERA FIC S.A.E.C.A."),
+        ("Interfisa Banco", "Interfisa Banco"),
+        ("Solar Banco S.A.E.", "Solar Banco S.A.E."),
+        ("Sudameris Bank S.A.E.C.A.", "Sudameris Bank S.A.E.C.A."),
+        ("TIGO-349","TIGO-349"),
+        ("TIGO-722","TIGO-722"),
+        ("TIGO-757","TIGO-757"),
+        ("Tu Financiera", "Tu Financiera"),
+        ("Ueno", "Ueno"),
+        ("Visión Banco S.A.E.C.A.", "Visión Banco S.A.E.C.A."),
+        ("WALLY-349", "WALLY-349"),
+        ("WALLY-722", "WALLY-722"),
+        ("WALLY-757", "WALLY-757"),
+        ("ZETA Banco", "ZETA Banco"),
+        ("ZIMPLE-349", "ZIMPLE-349"),
+        ("ZIMPLE-722", "ZIMPLE-722"),
+        ("ZIMPLE-757", "ZIMPLE-757"),
+    ]
+
+    estado = [
+        ("P", "Pendiente"),
+        ("A", "Aprobado"),
+        ("R", "Rechazado"),
+    ]
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    entidad = models.CharField(choices=entidades,max_length=100)
+    remitente = models.CharField(
+        max_length=50, default="El cliente es el remitente", blank=True, null=True
+    )
+    num_comprobante = models.CharField(max_length=50, blank=True, null=True)
+    agregado = models.DateTimeField(auto_now_add=True)
+    monto = models.PositiveIntegerField(default=0)
+    imagen_comprobante = models.ImageField(
+        upload_to="comprobantes", blank=True, null=True
+    )
+    estado = models.CharField(max_length=1, choices=estado, default="P")
+    observacion = models.TextField(blank=True, null=True)
+
+    def clean(self):
+        if self.num_comprobante:
+            if Transaccion.objects.filter(
+                num_comprobante=self.num_comprobante
+            ).exists():
+                raise ValidationError(
+                    {"num_comprobante": "El número de comprobante ya existe."}
+                )
+
+    def __str__(self):
+        return f"{self.cliente}"

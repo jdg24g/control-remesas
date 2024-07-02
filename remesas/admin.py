a54a01302f14353e87cb6c88948f228254a2a4db
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2024-07-02 16:14:58 -0400

    fix change ci and num_comp

--- remesas/admin.py
+++ remesas/admin.py
@@ -1,3 +1,4 @@
+# admin.py
 from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
@@ -6,22 +7,18 @@ from django.utils import timezone
 from .models import Cliente, Transaccion
 from django.core.exceptions import ValidationError
 
-
 class MonthListFilter(admin.SimpleListFilter):
     title = _("mes")
     parameter_name = "month"
 
     def lookups(self, request, model_admin):
-        months = [
-            (i, timezone.now().replace(month=i).strftime("%B")) for i in range(1, 13)
-        ]
+        months = [(i, timezone.now().replace(month=i).strftime("%B")) for i in range(1, 13)]
         return months
 
     def queryset(self, request, queryset):
         if self.value():
             return queryset.filter(agregado__month=self.value())
         return queryset
-
 
 class YearListFilter(admin.SimpleListFilter):
     title = _("año")
@@ -37,25 +34,18 @@ class YearListFilter(admin.SimpleListFilter):
             return queryset.filter(agregado__year=self.value())
         return queryset
 
-
 class MultiSelectEntityFilter(admin.SimpleListFilter):
     title = _("entidad")
     parameter_name = "entidad"
 
     def lookups(self, request, model_admin):
-        entidades = set(Transaccion.objects.values_list("entidad", flat=True))
+        entidades = Transaccion.objects.values_list("entidad", flat=True).distinct()
         return [(entidad, entidad) for entidad in entidades]
 
     def queryset(self, request, queryset):
         if self.value():
-            entidades = self.value()
-            return queryset.filter(entidad__in=entidades)
+            return queryset.filter(entidad__in=self.value().split(","))
         return queryset
-
-    def value(self):
-        value = super().value()
-        return value.split(",") if value else []
-
 
 class ClienteForm(forms.ModelForm):
     class Meta:
@@ -64,15 +54,9 @@ class ClienteForm(forms.ModelForm):
 
     def clean_cedula(self):
         cedula = self.cleaned_data.get("cedula")
-        if (
-            cedula
-            and Cliente.objects.filter(cedula=cedula)
-            .exclude(pk=self.instance.pk)
-            .exists()
-        ):
+        if cedula and Cliente.objects.filter(cedula=cedula).exclude(pk=self.instance.pk).exists():
             raise ValidationError("La cédula ya existe.")
         return cedula
-
 
 class TransaccionForm(forms.ModelForm):
     class Meta:
@@ -81,104 +65,76 @@ class TransaccionForm(forms.ModelForm):
 
     def clean_num_comprobante(self):
         num_comprobante = self.cleaned_data.get("num_comprobante")
-        if (
-            num_comprobante
-            and Transaccion.objects.filter(num_comprobante=num_comprobante).exists()
-        ):
-            raise ValidationError("El número de comprobante ya existe.")
+        if num_comprobante:
+            transaccion_qs = Transaccion.objects.filter(num_comprobante=num_comprobante)
+            if self.instance.pk:
+                transaccion_qs = transaccion_qs.exclude(pk=self.instance.pk)
+            if transaccion_qs.exists():
+                raise ValidationError("El número de comprobante ya existe.")
         return num_comprobante
-
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
+    form = ClienteForm
     list_display = ["cedula", "nombre", "apellido", "whatsapp"]
     search_fields = ["cedula", "nombre", "apellido"]
-
 
 @admin.register(Transaccion)
 class TransaccionAdmin(admin.ModelAdmin):
     form = TransaccionForm
     list_display = [
-        "cliente",
-        "observacion",
-        "entidad",
-        "monto",
-        "estado",
-        "caja",
-        "imagen_comprobante_preview",
-        "cliente_whatsapp",
+        "cliente", "observacion", "entidad", "monto", "estado", "caja", "imagen_comprobante_preview", "cliente_whatsapp"
     ]
-    list_filter = ["caja","estado", MultiSelectEntityFilter, YearListFilter, MonthListFilter]
-    search_fields = [
-        "cliente__cedula",
-        "cliente__nombre",
-        "cliente__apellido",
-        "entidad",
-    ]
+    list_filter = ["caja", "estado", MultiSelectEntityFilter, YearListFilter, MonthListFilter]
+    search_fields = ["cliente__cedula", "cliente__nombre", "cliente__apellido", "entidad"]
     autocomplete_fields = ["cliente"]
 
     def imagen_comprobante_preview(self, obj):
         if obj.imagen_comprobante:
             return format_html(
-                '<a href="{0}" class="preview-link" target="_blank" data-toggle="modal" data-target="#comprobanteModal"><img src="{0}" width="50" height="50" /></a>',
+                '<a href="{0}" class="preview-link" target="_blank"><img src="{0}" width="50" height="50" /></a>',
                 obj.imagen_comprobante.url,
             )
         return "Sin Foto"
-
     imagen_comprobante_preview.short_description = "Comprobante"
 
     def cliente_whatsapp(self, obj):
         return format_html(
-            '<a href="https://wa.me/{0}"target="_blank">Enviar Whatsapp</a>',
+            '<a href="https://wa.me/{0}" target="_blank">Enviar Whatsapp</a>',
             obj.cliente.whatsapp,
         )
 
-    actions = ["marcar_aprobadas", "marcar_rechazadas", "marcar_pendientes","marcar_enviados","marcarComoGiro", "marcarComoInterfisa"]
+    actions = ["marcar_aprobadas", "marcar_rechazadas", "marcar_pendientes", "marcar_enviados", "marcar_como_giro", "marcar_como_interfisa"]
 
     def marcar_aprobadas(self, request, queryset):
         updated = queryset.update(estado="A")
-        self.message_user(
-            request, f"Se marcaron como Aprobadas {updated} transacciones."
-        )
-
+        self.message_user(request, f"Se marcaron como Aprobadas {updated} transacciones.")
     marcar_aprobadas.short_description = _("Marcar como Aprobadas")
 
     def marcar_rechazadas(self, request, queryset):
         updated = queryset.update(estado="R")
-        self.message_user(
-            request, f"Se marcaron como Rechazadas {updated} transacciones."
-        )
-
+        self.message_user(request, f"Se marcaron como Rechazadas {updated} transacciones.")
     marcar_rechazadas.short_description = _("Marcar como Rechazadas")
 
     def marcar_pendientes(self, request, queryset):
         updated = queryset.update(estado="P")
-        self.message_user(
-            request, f"Se marcaron como Pendientes {updated} transacciones."
-        )
-
+        self.message_user(request, f"Se marcaron como Pendientes {updated} transacciones.")
     marcar_pendientes.short_description = _("Marcar como Pendientes")
 
-    def marcar_enviados(self, request,queryset):
+    def marcar_enviados(self, request, queryset):
         updated = queryset.update(estado="E")
-        self.message_user(
-            request, f"Se marcaron como Enviados {updated} transacciones."
-        )
+        self.message_user(request, f"Se marcaron como Enviados {updated} transacciones.")
     marcar_enviados.short_description = _("Marcar como Enviados")
 
-    def marcarComoGiro(self,request, queryset):
+    def marcar_como_giro(self, request, queryset):
         updated = queryset.update(caja="G")
-        self.message_user(
-            request, f"Se marcaron como Giro {updated} transacciones."
-        )
-    marcarComoGiro.short_description = _("Marcar como Giro")
+        self.message_user(request, f"Se marcaron como Giro {updated} transacciones.")
+    marcar_como_giro.short_description = _("Marcar como Giro")
 
-    def marcarComoInterfisa(self, request, queryset):
+    def marcar_como_interfisa(self, request, queryset):
         updated = queryset.update(caja="I")
-        self.message_user(
-            request, f"Se marcaron como Interfisa {updated} transacciones."
-        )
-    marcarComoInterfisa.short_description = _("Marcar como Interfisa")
+        self.message_user(request, f"Se marcaron como Interfisa {updated} transacciones.")
+    marcar_como_interfisa.short_description = _("Marcar como Interfisa")
 
 admin.site.site_header = "PLAYNET"
 admin.site.site_title = "PLAYNET"
--- remesas/models.py
+++ remesas/models.py
@@ -1,23 +1,18 @@
+# models.py
 from django.db import models
 from django.core.exceptions import ValidationError
-
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50, blank=True, null=True)
-    cedula = models.CharField(max_length=20)
+    cedula = models.CharField(max_length=20, unique=True)
     whatsapp = models.CharField(max_length=20, blank=True, null=True)
-
-    def clean(self):
-        if Cliente.objects.filter(cedula=self.cedula).exists():
-            raise ValidationError("Cedula ya existe")
 
     def __str__(self):
         return f"{self.cedula} {self.nombre.upper()} {self.apellido.upper()}"
 
-
 class Transaccion(models.Model):
-    entidades = [
+    ENTIDADES = [
         ("BANCOP S.A.", "BANCOP S.A."),
         ("Banco Atlas S.A.", "Banco Atlas S.A."),
         ("Banco BASA", "Banco BASA"),
@@ -35,19 +30,13 @@ class Transaccion(models.Model):
         ("Cooperativa Coodeñe Ltda.", "Cooperativa Coodeñe Ltda."),
         ("Cooperativa Colonias Unidas Ltda.", "Cooperativa Colonias Unidas Ltda."),
         ("Cooperativa Coomecipar Ltda.", "Cooperativa Coomecipar Ltda."),
-        (
-            "Cooperativa de las Fuerzas Armadas de la Nación Ltda.",
-            "Cooperativa de las Fuerzas Armadas de la Nación Ltda.",
-        ),
+        ("Cooperativa de las Fuerzas Armadas de la Nación Ltda.", "Cooperativa de las Fuerzas Armadas de la Nación Ltda."),
         ("Cooperativa Fernheim Ltda.", "Cooperativa Fernheim Ltda."),
         ("Cooperativa Mborayhu Ltda.", "Cooperativa Mborayhu Ltda."),
         ("Cooperativa Mburicao Ltda.", "Cooperativa Mburicao Ltda."),
         ("Cooperativa Medalla Milagrosa Ltda.", "Cooperativa Medalla Milagrosa Ltda."),
         ("Cooperativa Mercado 4 Ltda.", "Cooperativa Mercado 4 Ltda."),
-        (
-            "Cooperativa Multiactiva 8 de Marzo Ltda.",
-            "Cooperativa Multiactiva 8 de Marzo Ltda.",
-        ),
+        ("Cooperativa Multiactiva 8 de Marzo Ltda.", "Cooperativa Multiactiva 8 de Marzo Ltda."),
         ("Cooperativa Naranjal Ltda.", "Cooperativa Naranjal Ltda."),
         ("Cooperativa Nazareth Ltda.", "Cooperativa Nazareth Ltda."),
         ("Cooperativa Neuland Ltda.", "Cooperativa Neuland Ltda."),
@@ -57,10 +46,7 @@ class Transaccion(models.Model):
         ("Cooperativa San Juan Bautista Ltda.", "Cooperativa San Juan Bautista Ltda."),
         ("Cooperativa Universitaria Ltda.", "Cooperativa Universitaria Ltda."),
         ("Cooperativa Ypacarai Ltda.", "Cooperativa Ypacarai Ltda."),
-        (
-            "Financiera Paraguayo Japonesa S.A.E.C.A",
-            "Financiera Paraguayo Japonesa S.A.E.C.A",
-        ),
+        ("Financiera Paraguayo Japonesa S.A.E.C.A", "Financiera Paraguayo Japonesa S.A.E.C.A"),
         ("FINANCIERA FIC S.A.E.C.A.", "FINANCIERA FIC S.A.E.C.A."),
         ("Interfisa Banco", "Interfisa Banco"),
         ("Solar Banco S.A.E.", "Solar Banco S.A.E."),
@@ -79,39 +65,36 @@ class Transaccion(models.Model):
         ("ZIMPLE-722", "ZIMPLE-722"),
         ("ZIMPLE-757", "ZIMPLE-757"),
     ]
-    cajas = [
+    CAJAS = [
         ("G", "Giro"),
         ("I","Interfisa")
     ]
-    estado = [
+    ESTADOS = [
         ("P", "Pendiente"),
         ("A", "Aprobado"),
         ("E", "Enviado"),
         ("R", "Rechazado"),
     ]
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    caja = models.CharField(choices=cajas, max_length=50,blank=True, null=True)
-    entidad = models.CharField(choices=entidades,max_length=100)
-    remitente = models.CharField(
-        max_length=50, default="El cliente es el remitente", blank=True, null=True
-    )
+    caja = models.CharField(choices=CAJAS, max_length=50, blank=True, null=True)
+    entidad = models.CharField(choices=ENTIDADES, max_length=100)
+    remitente = models.CharField(max_length=50, default="El cliente es el remitente", blank=True, null=True)
     num_comprobante = models.CharField(max_length=50, blank=True, null=True)
     agregado = models.DateTimeField(auto_now_add=True)
     monto = models.PositiveIntegerField(default=0)
-    imagen_comprobante = models.ImageField(
-        upload_to="comprobantes", blank=True, null=True
-    )
-    estado = models.CharField(max_length=1, choices=estado, default="P")
+    imagen_comprobante = models.ImageField(upload_to="comprobantes", blank=True, null=True)
+    estado = models.CharField(max_length=1, choices=ESTADOS, default="P")
     observacion = models.TextField(blank=True, null=True)
 
     def clean(self):
         if self.num_comprobante:
-            if Transaccion.objects.filter(
-                num_comprobante=self.num_comprobante
-            ).exists():
-                raise ValidationError(
-                    {"num_comprobante": "El número de comprobante ya existe."}
-                )
+            transaccion_qs = Transaccion.objects.filter(num_comprobante=self.num_comprobante)
+            if self.pk:
+                transaccion_qs = transaccion_qs.exclude(pk=self.pk)
+                print(transaccion_qs)
+            if transaccion_qs.exists():
+                print(transaccion_qs)
+                raise ValidationError({"num_comprobante": "El número de comprobante ya existe."})
 
     def __str__(self):
         return f"{self.cliente}"
